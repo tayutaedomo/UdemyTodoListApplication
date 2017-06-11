00b85604d3ddc0073e1007b998e05d8fb9d99110
tayutaedomo <tayutaedomo@gmail.com>
2017-06-11 17:53:22 +0900

    Add Realm configuration

--- app/src/main/java/net/tayutaedomo/udemytodolistapplication/MainActivity.java
+++ app/src/main/java/net/tayutaedomo/udemytodolistapplication/MainActivity.java
@@ -9,6 +9,9 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -26,27 +29,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        RealmConfiguration realmConfig = new RealmConfiguration.Builder().build();
+        Realm realm = Realm.getInstance(realmConfig);
     }
 }
+
